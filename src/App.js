2cc84f7f617b8fae853def6972005bedd315631e
josesales <salesbass@gmail.com>
2021-09-06 12:49:53 +0200

    Adding initial files and archtecture

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Loader from './components/Loader';
+import Home from './pages/Home';
+import "./styles/main.scss";
 
-function App() {
+const MovieDetails = lazy(() => import('./pages/MovieDetails'));
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+
+      <Header />
+        <BrowserRouter>
+        <Switch>
+          <Suspense fallback={<Loader />}>
+            <Route exact path='/' component={Home} />
+            <Route path='/movieDetails' component={MovieDetails} />
+          </Suspense>
+        </Switch>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
---
+++ src/pages/MovieDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MovieDetails = () => {
+
+    return (
+        <div className="details">
+            <h1>Movie Details Page</h1>
+        </div>
+    );
+}
+
+export default MovieDetails;
