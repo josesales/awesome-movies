b854a725b5534970aef02235dcf9343f87d3e20f
Jose Sales <salesbass@gmail.com>
2024-12-22 18:37:55 -0300

    Improving performance with debounce

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,73 +1,89 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import InternalError from '../components/InternalError';
-import Loader from '../components/Loader';
-import MovieItems from '../components/MovieItems';
-import HTML_ENTITIES from '../util/htmlEntities';
-import { get } from '../util/requestSender';
+import React, { useCallback, useEffect, useState } from "react";
+import InternalError from "../components/InternalError";
+import Loader from "../components/Loader";
+import MovieItems from "../components/MovieItems";
+import HTML_ENTITIES from "../util/htmlEntities";
+import { get } from "../util/requestSender";
+import { useDebounce } from "../hooks/useDebounce";
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [mounted, setMounted] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [text, setText] = useState("");
+  const [internalError, setInternalError] = useState(false);
+  const debounce = useDebounce(1000);
 
-    const [loading, setLoading] = useState(false);
-    const [movies, setMovies] = useState([]);
-    const [text, setText] = useState('');
-    const [internalError, setInternalError] = useState(false);
+  const search = useCallback(async (currentText) => {
+    try {
+      let results = null;
+      setLoading(true);
 
-    const searchMovies = useCallback((currentText) => {
+      if (currentText && currentText.trim()) {
+        const data = await get(
+          `search/movie?page=1&include_adult=false&query=${currentText}`
+        );
+        results = data.results;
+      } else {
+        const data = await get(`trending/movie/week?page=1`);
+        results = data.results;
+      }
+      setMovies(results);
+      setLoading(false);
+    } catch (error) {
+      setInternalError(true);
+    }
+  }, []);
 
-        const search = async () => {
+  const searchMovie = debounce((currentText) => {
+    search(currentText);
+  });
 
-            try {
-                let results = null;
-                setLoading(true);
-        
-                if(currentText && currentText.trim()) {
-        
-                    const data = await get(`search/movie?page=1&include_adult=false&query=${currentText}`);
-                    results = data.results;
-                }else {
-                    const data = await get(`trending/movie/week?page=1`);
-                    results = data.results;
-                }
-                setMovies(results);
-                setLoading(false)
-            }catch(error) {
-                setInternalError(true);
-            }
-        }
+  useEffect(() => {
+    search("");
+    setMounted(true);
+  }, [search]);
 
-        search();
-    }, [])
+  const onTextChange = useCallback(
+    (event) => {
+      const currentText = event.target.value;
 
-    useEffect(() => {
-        searchMovies('');
-    }, [searchMovies]);
+      setText(currentText);
 
-    const onTextChange = useCallback(event => {
+      searchMovie(currentText);
+    },
+    [searchMovie]
+  );
 
-        const currentText = event.target.value;
+  return (
+    <React.Fragment>
+      {internalError ? (
+        <InternalError />
+      ) : (
+        <div className="home">
+          <input
+            type="text"
+            placeholder={HTML_ENTITIES.search}
+            onChange={onTextChange}
+            className="input"
+            value={text}
+          />
 
-        setText(currentText);
-        searchMovies(currentText);
-    }, [searchMovies]);
-
-
-    return (
-        <React.Fragment>
-            {
-                internalError ? <InternalError /> :
-
-                <div className="home">
-                    <input type="text" placeholder={HTML_ENTITIES.search} onChange={onTextChange} className="input" value={text}/>
-
-                    {
-                        loading ? <Loader /> : 
-                            movies?.length > 0 ? <MovieItems movies={movies} /> :
-                                <span className="section-span section-margin-2">Movie not found!</span>
-                    }
-                </div>
-            }
-        </React.Fragment>
-    );
-}
+          {loading ? (
+            <Loader />
+          ) : movies?.length > 0 ? (
+            <MovieItems movies={movies} />
+          ) : (
+            mounted && (
+              <span className="section-span section-margin-2">
+                Movie not found!
+              </span>
+            )
+          )}
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
 
 export default Home;
